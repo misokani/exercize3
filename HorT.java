038b030eaee8e013e27df8d9eb9bb6ff3a6a539e
misokani <kuri3don@gmail.com>
2024-07-04 15:23:02 +0900

    User name added

--- HorT.java
+++ HorT.java
@@ -3,9 +3,16 @@ import java.util.Scanner;
 class HorT{
     public static void main(String[] args) {
         Random rnd = new Random();
+        String name ;
+        Scanner sc = new Scanner(System.in);
         int head =0;
         int tail = 0;
         int coun = 0;
+        System.out.println("Who are you?");
+        System.out.print("> ");
+        name = sc.next();
+        System.out.println("Hello, " + name + "!");
+
         System.out.println("Tossing a coin...");
         while(head<2 && tail<2){
             coun++;
